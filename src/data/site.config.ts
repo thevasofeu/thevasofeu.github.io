75987181c218ec35ca20b5f5815cf5941ab4b4bc
Admir <theva.click@outlook.com>
2024-08-24 15:34:16 +0200

    Setup & commit

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
+	author: 'Admir Zukic', // Site author
+	title: 'The VAs of EU', // Site title.
+	description: 'Your Home Office like in the EU.', // Description to display in the meta tags
+	lang: 'en-US',
+	ogLocale: 'en_US',
 	shareMessage: 'Share this post', // Message to share a post on social media
-	paginationSize: 6 // Number of posts per page
+	paginationSize: 7 // Number of posts per page
 }
